38453a39bd4193b993ab067f560490966bf225bc
dean-kim <keunu07@gmail.com>
2018-11-05 13:01:56 +0900

    some test

--- Chapter1-1/cmake-build-debug/CMakeFiles/Chapter1_1.dir/DependInfo.cmake
+++ Chapter1-1/cmake-build-debug/CMakeFiles/Chapter1_1.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/KEUNU/study/cpp/c-study/Chapter1_1/main.cpp" "/Users/KEUNU/study/cpp/c-study/Chapter1_1/cmake-build-debug/CMakeFiles/Chapter1_1.dir/main.cpp.o"
+  "/Users/KEUNU/study/cpp/c-study/Chapter1-1/main.cpp" "/Users/KEUNU/study/cpp/c-study/Chapter1-1/cmake-build-debug/CMakeFiles/Chapter1_1.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
