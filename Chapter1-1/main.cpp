567cf3e9b1308b96e565c2a147365d21f9c7b8fb
dean-kim <keunu07@gmail.com>
2018-11-06 08:40:19 +0900

    basic of operator

--- Chapter1-1/main.cpp
+++ Chapter1-1/main.cpp
@@ -6,19 +6,22 @@
 
 using namespace std;
 
-void doSomthing(int x)
-{
-    x = 123;
-    cout << x << endl; // #2
-}
-
 int main()
 {
-    int x = 0;
+    int x = 2; // x is a variable, 2 is a literal.
+    int y = (x > 0) ? 1 : 2; // 삼항 연산자 = 조건 ? 참 : 거짓
 
-    cout << x << endl; // #1
-    doSomthing(x);
-    cout << x << endl; // #3
+    cout << y << endl;
+
+    cout << 1 + 2 << endl; // "1 + 2" is a expression
+
+    cout << x + 2 << endl; // "+" is a operator, "x, 2" is a operand
+
+    cout << -x << endl; // 단항 연산자
+
+    cout << x << 1 + 2 << &x << endl; // 이항 연산자
+
+    cout << "Hello World" << endl;
 
     return 0;
 }
