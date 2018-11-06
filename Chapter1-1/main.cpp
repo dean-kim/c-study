bd8e53e6cee0c0fffb17d9d051678cb93df766e6
dean-kim <keunu07@gmail.com>
2018-11-06 10:20:58 +0900

    preprocessor

--- Chapter1-1/main.cpp
+++ Chapter1-1/main.cpp
@@ -5,29 +5,14 @@
 #include <iostream>
 #include "add.h"
 
-
-namespace MySpace1
-{
-    int doSomething(int a, int b)
-    {
-        return a + b;
-    }
-}
-
-int doSomething(int a, int b)
-{
-    return a * b;
-}
-
-
 using namespace std;
 
+#define MY_NUMBER(a, b) ((a > b) ? a : b)
 
 int main()
 {
-    cout << MySpace1::doSomething(1, 2) << endl;
 
-    cout << doSomething(1, 2) << endl;
+    cout << MY_NUMBER(2, 3) << endl;
 
     return 0;
 }
