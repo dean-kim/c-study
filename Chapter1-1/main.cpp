58421c38a73fb727637a2a230be7a6e7e6ddbb52
dean-kim <keunu07@gmail.com>
2018-11-07 11:53:01 +0900

    sizeof, comma operator, conditional operator

--- Chapter1-1/main.cpp
+++ Chapter1-1/main.cpp
@@ -10,14 +10,29 @@ using namespace std;
 int main()
 {
 
-    int x = 6, y = 6;
+    // comma operator
+    int x = 3;
+    int y = 10;
+    int z = (++x, ++y);
+    int zz = (x++, x + y);
+
+    cout << x << " " << y << " " << z << endl;
+    cout << zz << endl;
+
+    // conditional operator (arithmetric if)
+    bool onSale = true;
+//    if(onSale)
+//        price = 10;
+//    else
+//        price = 100;
+//
+
+    // same result above if statement
+    const int price = (onSale == true)? 10 : 100;
 
 
-    cout << x << " " << y << endl;
-    cout << ++x << " " << --y << endl;
-    cout << x << " " << y << endl;
-    cout << x++ << " " << y-- << endl;
-    cout << x << " " << y << endl;
+    cout << price << endl;
+
 
     return 0;
 
