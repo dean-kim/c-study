003163971724567db91f85f5ca2141f7fa84f738
dean-kim <keunu07@gmail.com>
2018-11-09 13:15:39 +0900

    auto keyword & data type inference

--- Chapter1-1/main.cpp
+++ Chapter1-1/main.cpp
@@ -5,17 +5,12 @@
 #include <iostream>
 #include <bitset>
 
-namespace a
-{
-    int my_var(10);
-    int my_a(123);
-    int count(123123123);
-}
+// template
 
-namespace b
+
+auto add(int x, int y) -> int // trailing
 {
-    int my_var(20);
-    int my_b(456);
+    return x + y;
 }
 
 int main()
@@ -23,24 +18,12 @@ int main()
 
     using namespace std;
 
-//    using std::cout;
-//    using std::endl;
-
-    cout << "Hello" << endl;
-
-    {
-        using namespace a;
-        cout << my_var << endl;
-//        cout << count << endl; // duplicated with count in std
-    }
-
-    {
-        using namespace b;
-        cout << b::my_var << endl;
-    }
-
-//    cout << my_a << endl;
-//    cout << my_b << endl;
+    // auto must use with initialization
+//    auto a; // not working
+    auto a = 123;
+    auto b = 123.0;
+    auto c = 1 + 2.0;
+    auto result = add(1, 2);
 
     return 0;
 
