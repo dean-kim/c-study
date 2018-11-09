9617bc8226dc34f1a8b445cc2cdaed73a5f666ee
dean-kim <keunu07@gmail.com>
2018-11-09 09:55:40 +0900

    Bit masks

--- Chapter1-1/main.cpp
+++ Chapter1-1/main.cpp
@@ -11,69 +11,29 @@ using namespace std;
 int main()
 {
 
-    const unsigned char opt0 = 1 << 0;
-    const unsigned char opt1 = 1 << 1;
-    const unsigned char opt2 = 1 << 2;
-    const unsigned char opt3 = 1 << 3;
+    const unsigned int red_mask = 0xFF0000;
+    const unsigned int green_mask = 0x00FF00;
+    const unsigned int blue_mask = 0x0000FF;
 
-    cout << bitset<8>(opt0) << endl;
-    cout << bitset<8>(opt1) << endl;
-    cout << bitset<8>(opt2) << endl;
-    cout << bitset<8>(opt3) << endl;
 
-    unsigned char items_flag = 0;
+    cout << std::bitset<32>(red_mask) << endl;
+    cout << std::bitset<32>(green_mask) << endl;
+    cout << std::bitset<32>(blue_mask) << endl;
 
-    cout << "No item" << bitset<8>(items_flag) << endl;
+    unsigned int pixel_color = 0xDAA520;
 
-    // item0 on
-    items_flag |= opt0;
+    cout << std::bitset<32>(pixel_color) << endl;
 
-    cout << "item0 obtained " << bitset<8>(items_flag) << endl;
+    unsigned char red = (pixel_color & red_mask) >> 16;
+    unsigned char green = (pixel_color & green_mask) >> 8;
+    unsigned char blue = pixel_color & blue_mask;
 
-    // item3 on
-    items_flag |= opt3;
+    cout << "red " << std::bitset<8>(red) << " " << int(red) << endl;
+    cout << "green " << std::bitset<8>(green) << " " << int(green) << endl;
+    cout << "blue " << std::bitset<8>(blue) << " " << int(blue) << endl;
 
-    cout << "item3 obtained " << bitset<8>(items_flag) << endl;
 
-    // item3 lost
-    items_flag &= ~opt3;
-
-    cout << "item3 lost " << bitset<8>(items_flag) << endl;
-
-    // has item0?
-    if (items_flag & opt0)
-    {
-        cout << "Has item0" << endl;
-    }
-    else
-        {
-        cout << "Not has item0" << endl;
-    }
-
-    // has item1?
-    if (items_flag & opt1)
-    {
-        cout << "Has item1" << endl;
-    }
-    else
-    {
-        cout << "Not has item1" << endl;
-    }
-
-    // obtain item 2, 3
-    items_flag |= (opt2 | opt3);
-
-    cout << bitset<8>(opt2 | opt3) << endl;
-    cout << "item2, 3 obtained " << bitset<8>(items_flag) << endl;
-
-    if ((items_flag & opt2) && !(items_flag & opt1))
-    {
-//        items_flag ^= opt2;
-//        items_flag ^= opt1;
-        items_flag ^= (opt2 | opt1);
-    }
-
-    cout << bitset<8>(items_flag) << endl;
+//    unsigned char red, green, blue;
 
     return 0;
 
