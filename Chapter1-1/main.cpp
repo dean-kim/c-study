92d9746f9663e93112db9ca4dabdc820d40d6914
dean-kim <keunu07@gmail.com>
2018-11-12 09:50:23 +0900

    about enumerated types index assign

--- Chapter1-1/main.cpp
+++ Chapter1-1/main.cpp
@@ -8,12 +8,21 @@
 
 using namespace std;
 
+enum Color // user defined data types
+{
+    COLOR_BLACK = -3,
+    COLOR_RED,
+    COLOR_BLUE = 5,
+    COLOR_GREEN,
+    COLOR_SKYBLUE,
+};
+
 int main()
 {
 
-    string a("Hello, World");
+    Color my_color = COLOR_BLACK;
 
-    cout << a.length() << endl;
+    cout << my_color << " " << COLOR_RED<< " " << COLOR_BLUE<< " " << COLOR_GREEN << endl;
 
     return 0;
 
