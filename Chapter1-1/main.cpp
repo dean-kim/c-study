6a2edaacb985f89c85dfe741c7fc8d5ee3611cfc
dean-kim <keunu07@gmail.com>
2018-11-13 08:56:39 +0900

    while loop-nested while

--- Chapter1-1/main.cpp
+++ Chapter1-1/main.cpp
@@ -13,15 +13,18 @@ using namespace std;
 int main()
 {
 
-    // this is infinite loop, because of overflow
-//    unsigned int count = 10;
-//
-//    while (count >= 0)
-//    {
-//        if (count == 0) cout << "0";
-//        else cout << count << " ";
-//        count--;
-//    }
+    int outer_count = 1;
+
+    while(outer_count <=5)
+    {
+        int inner_count = 1;
+        while (inner_count <= outer_count)
+        {
+            cout << inner_count++ << " ";
+        }
+        cout << endl;
+        ++outer_count;
+    }
 
     return 0;
 
