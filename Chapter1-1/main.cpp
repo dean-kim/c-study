6263a62037ca77fa4b12e66e6c36c4e5f0e6dc26
dean-kim <keunu07@gmail.com>
2018-11-12 09:21:24 +0900

    std string intro-3

--- Chapter1-1/main.cpp
+++ Chapter1-1/main.cpp
@@ -4,22 +4,24 @@
 
 #include <iostream>
 #include <string>
-
+#include <limits>
 
 using namespace std;
 
 int main()
 {
 
+    cout << "your age ? : ";
+    int age;
+    cin >> age;
+    //std::getline(std::cin, age);
+    //std::cin.ignore(32767, '\n');
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
     cout << "Your name ? : ";
     string name;
     //cin >> name;
     std::getline(std::cin, name);
-
-    cout << "your age ? : ";
-    string age;
-    //cin >> age;
-    std::getline(std::cin, age);
 
     cout << name << " " << age <<endl;
 
