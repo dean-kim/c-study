4ae7539642b80639816f91e845a5bec208e5c1c4
dean-kim <keunu07@gmail.com>
2018-11-13 08:16:55 +0900

    switch case-1

--- Chapter1-1/main.cpp
+++ Chapter1-1/main.cpp
@@ -20,36 +20,41 @@ enum class Colors
 
 void printColorName(Colors color)
 {
-//    if (color == Colors::BLACK)
-//        cout << "Black" << endl;
-//    else if (color == Colors::WHITE)
-//        cout << "White" << endl;
-//
-
+    switch (static_cast<int>(color))
+    {
+        case 0 :
+            cout << "Black" << endl;
+            break;
+        case 1 :
+            cout << "White" << endl;
+            break;
+    }
 
 }
 
 int main()
 {
-//    printColorName(Colors::BLACK);
-    int x;
-    cin >> x;
+    printColorName(Colors::BLACK);
+    printColorName(Colors::WHITE);
 
-    {
-        switch (x)
-        {
-            case 0:
-                cout << "Zero";
-                break;
-            case 1:
-                cout << "One";
-                break;
-            case 2:
-                cout << "Two";
-                break;
-        }
-        cout << endl;
-    }
+//    int x;
+//    cin >> x;
+//
+//    {
+//        switch (x)
+//        {
+//            case 0:
+//                cout << "Zero";
+//                break;
+//            case 1:
+//                cout << "One";
+//                break;
+//            case 2:
+//                cout << "Two";
+//                break;
+//        }
+//        cout << endl;
+//    }
 
     return 0;
 
