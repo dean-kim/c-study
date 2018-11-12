f7c777783b425e906e3cb9da14653fabed7a115c
dean-kim <keunu07@gmail.com>
2018-11-12 13:05:11 +0900

    if statement-4

--- Chapter1-1/main.cpp
+++ Chapter1-1/main.cpp
@@ -13,15 +13,17 @@ using namespace std;
 int main()
 {
 
-    int x, y;
-    cin >> x >> y;
+    int x;
+    cin >> x;
 
-    if (x > 0 && y > 0)
-        cout << "both numbers are positive" << endl;
-    else if (x > 0 || y > 0)
-        cout << "one of the numbers is positive" << endl;
-    else
-        cout << "Neither number is positive" << endl;
+    if (x > 10)
+        cout << "A" << endl;
+    else if (x == -1)
+        return 0; // same result with 'exit(0);'
+    else if (x < 0)
+        cout << "B" << endl;
+
+    cout << "Hello" << endl;
 
     return 0;
 
