404829e9aa72b95fd4fc9042aa746768d194afdf
dean-kim <keunu07@gmail.com>
2018-11-12 09:59:43 +0900

    enumerated types

--- Chapter1-1/main.cpp
+++ Chapter1-1/main.cpp
@@ -10,9 +10,9 @@ using namespace std;
 
 enum Color // user defined data types
 {
-    COLOR_BLACK = -3,
+    COLOR_BLACK,
     COLOR_RED,
-    COLOR_BLUE = 5,
+    COLOR_BLUE,
     COLOR_GREEN,
     COLOR_SKYBLUE,
 };
@@ -20,9 +20,30 @@ enum Color // user defined data types
 int main()
 {
 
-    Color my_color = COLOR_BLACK;
+    int color_id = COLOR_RED;
+    // not Working
+    // Color my_color = color_id;
+    // but can casting
+    Color my_color = static_cast<Color>(3);
 
-    cout << my_color << " " << COLOR_RED<< " " << COLOR_BLUE<< " " << COLOR_GREEN << endl;
+    cout << color_id << endl;
+    cout << my_color << endl;
+
+    int in_number;
+    cin >> in_number;
+
+    if(in_number == static_cast<Color>(0)) my_color = static_cast<Color>(0);
+
+    cout << my_color << endl;
+
+    string str_input;
+
+    std::getline(cin, str_input);
+
+    if(str_input == "COLOR_BLACK") my_color = static_cast<Color>(0);
+
+    cout << my_color << endl;
+
 
     return 0;
 
