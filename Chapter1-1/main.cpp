e7007474ac44ed4f50622a020d491c3ce115a32a
dean-kim <keunu07@gmail.com>
2018-11-12 11:02:54 +0900

    type alizses-1

--- Chapter1-1/main.cpp
+++ Chapter1-1/main.cpp
@@ -12,11 +12,14 @@ using namespace std;
 int main()
 {
 
-    typedef double distance_t;
+    typedef std::vector<std::pair<std::string, int> > pairlist_t;
+    //using pairlist_t = vector<pair<string, int> >;
 
-    double my_distance;
-    distance_t home2work;
-    distance_t home2school;
+    pairlist_t pairlist1;
+    //vector<pair<string, int> > pairlist1;
+
+    pairlist_t pairlist2;
+    //vector<pair<string, int> > pairlist2;
 
     return 0;
 
