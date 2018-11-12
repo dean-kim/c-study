bda874e5c52e29c7c431f77a49fcfe0f3bd7d947
dean-kim <keunu07@gmail.com>
2018-11-12 10:58:55 +0900

    type alizses

--- Chapter1-1/main.cpp
+++ Chapter1-1/main.cpp
@@ -4,37 +4,19 @@
 
 #include <iostream>
 #include <string>
-#include <limits>
+#include <vector>
+#include <cstdint>
 
 using namespace std;
 
 int main()
 {
 
-    enum class Color
-    {
-        RED,
-        BLUE,
-    };
+    typedef double distance_t;
 
-    enum class Fruit
-    {
-        BANANA,
-        APPLE,
-    };
-
-    Color color1 = Color::RED;
-    Color color2 = Color::BLUE;
-
-    Fruit fruit = Fruit::BANANA;
-
-//    if (color == fruit)
-//        cout << "Color is fruit ? " << endl;
-
-    if (color1 == color2)
-        cout << "Same color " << endl;
-    else
-        cout << "Different color" << endl;
+    double my_distance;
+    distance_t home2work;
+    distance_t home2school;
 
     return 0;
 
