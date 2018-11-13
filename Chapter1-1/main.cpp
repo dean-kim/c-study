13e6b1d8ab0bd4b3cd06985bb147ad4b280beae6
dean-kim <keunu07@gmail.com>
2018-11-13 13:29:20 +0900

    random numbers-2

--- Chapter1-1/main.cpp
+++ Chapter1-1/main.cpp
@@ -3,25 +3,20 @@
 //
 
 #include <iostream>
+#include <cstdlib> // std::radn(), std::srand()
+#include <ctime> // std::time()
 
 using namespace std;
 
-// Pseudo Random Number Generator
-unsigned int PRNG()
-{
-    // seed number
-    static unsigned int seed = 5523;
-
-    seed = 8253729 * seed + 2396403;
-
-    return seed % 32768;
-}
 
 int main()
 {
+    //std::srand(5323); // seed
+    std::srand(static_cast<unsigned int>(std::time(0)));
+
     for (int count = 1; count <= 100; ++count)
     {
-        cout << PRNG() << "\t";
+        cout << std::rand() << "\t";
 
         if (count % 5 == 0) cout << endl;
     }
