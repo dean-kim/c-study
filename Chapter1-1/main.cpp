135ce77bfdebc588e816794ea9124d4ec7ed0060
dean-kim <keunu07@gmail.com>
2018-11-13 15:22:47 +0900

    Array-7

--- Chapter1-1/main.cpp
+++ Chapter1-1/main.cpp
@@ -6,14 +6,34 @@
 
 using namespace std;
 
-#define NUM_STU 10000000
+#define NUM_STU 100000
+
+void doSomething(int stu[])
+{
+    cout << &stu << endl;
+    cout << stu[0] << endl;
+    cout << stu[1] << endl;
+    cout << stu[2] << endl;
+    cout << stu[3] << endl;
+    cout << sizeof(stu) << endl; // pointer size
+
+}
 
 int main()
 {
     const int num_stu = 5;
 //    cin >> num_stu;
 
-    int stu[num_stu];
+    int stu[num_stu] = { 1, 2, 3, 4, 5, };
+
+    cout << &stu << endl;
+    cout << stu[0] << endl;
+    cout << stu[1] << endl;
+    cout << stu[2] << endl;
+    cout << stu[3] << endl;
+    cout << sizeof(stu) << endl;
+
+    doSomething(stu);
 
     return 0;
 
