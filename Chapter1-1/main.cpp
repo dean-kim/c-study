ace6b35ab9aa5954dc28bcb7253b62c7271c5dc2
dean-kim <keunu07@gmail.com>
2018-11-13 13:06:46 +0900

    break / continue -1

--- Chapter1-1/main.cpp
+++ Chapter1-1/main.cpp
@@ -7,25 +7,14 @@
 using namespace std;
 
 
-void breakOrReturn()
-{
-    while (true)
-    {
-        char ch;
-        cin >> ch;
-
-        if (ch == 'b')
-            break;
-        if (ch == 'r')
-            return;
-    }
-    cout << "Hello" << endl;
-}
-
 int main()
 {
-    breakOrReturn();
 
+    for (int i = 0; i < 10; ++i)
+    {
+        cout << i << endl;
+        if (i == 5) break;
+    }
 
     return 0;
 
