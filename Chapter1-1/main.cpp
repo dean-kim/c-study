7d96ba80ce17fbb731de834d3dd08cf3c3794046
dean-kim <keunu07@gmail.com>
2018-11-15 13:54:44 +0900

    about c-style string method

--- Chapter1-1/main.cpp
+++ Chapter1-1/main.cpp
@@ -17,6 +17,14 @@ int main()
 
     strcpy(dest, source);
 
+//    strcat();
+
+//    strcat(dest, source);
+
+//    strcmp(); -> compare, if equal return 0, else return -1
+
+    cout << strcmp(source, dest) << endl;
+
     cout << source << endl;
     cout << dest << endl;
 
