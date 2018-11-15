908073302a4753a4a6afc6bbe7d7d95b5f2603b0
dean-kim <keunu07@gmail.com>
2018-11-15 13:37:24 +0900

    about char array

--- Chapter1-1/main.cpp
+++ Chapter1-1/main.cpp
@@ -10,42 +10,12 @@ using namespace std;
 int main()
 {
 
-    const int num_rows = 3;
-    const int num_columns = 5;
+    char myString[] = "string";
 
-    for (int row = 0; row < num_rows; ++row)
+    for (int i = 0; i < 7; ++i)
     {
-        for (int col = 0; col < num_columns; ++col)
-            cout << '[' << row << ']' << '[' << col << ']' << '\t';
-
-        cout << endl;
+        cout << (int)myString[i] << endl;
     }
-
-    cout << endl;
-
-
-//    int array[num_rows][num_columns] = { 0 };
-//    int array[5][4][3];
-    int array[num_rows][num_columns] = // row-major
-            {
-                    {1, 2, 3, 4, 5}, // row 0
-                    {6, 7, 8, 9, 10}, // row 1
-                    {11, 12, 13, 14, 15} // row 2
-            };
-
-    for (int row = 0; row < num_rows; ++row)
-    {
-        for (int col = 0; col < num_columns; ++col)
-//            cout << '[' << row << ']' << '[' << col << ']' << '\t';
-            cout << array[row][col] << '\t';
-
-        cout << endl;
-    }
-
-    cout << endl;
-
-
-
 
     return 0;
 
