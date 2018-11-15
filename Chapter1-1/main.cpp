ac5c76e86b1b1f7fefcd01286beb88cd6111ba75
dean-kim <keunu07@gmail.com>
2018-11-15 09:15:08 +0900

    make matrix

--- Chapter1-1/main.cpp
+++ Chapter1-1/main.cpp
@@ -23,6 +23,29 @@ int main()
 
     cout << endl;
 
+
+    int array[num_rows][num_columns] = // row-major
+            {
+                    {1, 2, 3, 4, 5}, // row 0
+                    {6, 7, 8, 9, 10}, // row 1
+                    {11, 12, 13, 14, 15} // row 2
+            };
+
+
+    for (int row = 0; row < num_rows; ++row)
+    {
+        for (int col = 0; col < num_columns; ++col)
+//            cout << '[' << row << ']' << '[' << col << ']' << '\t';
+            cout << array[row][col] << '\t';
+
+        cout << endl;
+    }
+
+    cout << endl;
+
+
+
+
     return 0;
 
 }
