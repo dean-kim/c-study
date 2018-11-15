c0ccaceb3ea21147041fea71db66eb374ed5c305
dean-kim <keunu07@gmail.com>
2018-11-15 13:47:13 +0900

    about char array-3

--- Chapter1-1/main.cpp
+++ Chapter1-1/main.cpp
@@ -12,11 +12,20 @@ int main()
 
     char myString[255];
 
-    cin >> myString;
+//    cin >> myString;
 
-    myString[4] = '\0';
+    cin.getline(myString, 255);
 
-    cout << myString << endl;
+//    cout << myString << endl;
+
+    int ix = 0;
+    while (true)
+    {
+        if(myString[ix] == '\0') break;
+        cout << myString[ix] << " " << (int)myString[ix] << endl;
+        ++ix;
+    }
+
 
     return 0;
 
