0dbbeeedca45d218dffdf4b79e5338521b9b12a9
dean-kim <keunu07@gmail.com>
2018-11-16 14:39:17 +0900

    static array & pointer

--- Chapter1-1/main.cpp
+++ Chapter1-1/main.cpp
@@ -8,52 +8,22 @@
 
 using namespace std;
 
-struct Something
-{
-    int a, b, c, d; // 4 * 4 = 16, but pointer is 8
-};
 
 int main()
 {
 
-    int x = 5;
-    double d = 123.0;
+    int array[5] = { 9, 7 ,5, 3, 1};
 
-//    typedef int* pint;
-//    pint ptr_x = &x, ptr_y = &x;
+    char name[] = "JackJack";
 
-    int *ptr_x = &x, *ptr_y = &x;
-    double *ptr_d = &d;
-    Something ss;
-    Something *ptr_s;
-
-    // pointer has data type
-    // 포인터도 결국 주소를 저장하는 변수에 불과하다.
-    // 포인터도 데이터 타입은 갖고 있음.
-    // int type 변수에 대한 포인터
-    // 포인터는 사이즈가 정해져있음, 모든 타입에 대해 포인터는 동일한 사이즈
-//    int * ptr_x = &x; // '*' is parameter
-
-    // address
-    cout << ptr_x << endl;
-    cout << ptr_d << endl;
-    cout << typeid(ptr_x).name() << endl;
+    cout << array << endl;
+    cout << name << endl;
+    cout << &array[0] << endl;
+    cout << &name[0] << endl;
 
     // de-referencing
-    cout << *ptr_x << endl;
-    cout << *ptr_d << endl;
-
-    // size check
-    cout << sizeof(x) << endl;
-    cout << sizeof(d) << endl;
-    cout << sizeof(ss) << endl;
-
-    cout << sizeof(&x) << " " << sizeof(ptr_x) << endl;
-    cout << sizeof(&d) << " " << sizeof(ptr_d) << endl;
-    cout << sizeof(&ss) << " " << sizeof(ptr_s) << endl;
-
-
-
+    cout << *array << endl;
+    cout << *name << endl;
 
     return 0;
 
