a96af944081bc8af26985cc26cb4831d7f5be541
dean-kim <keunu07@gmail.com>
2018-11-20 08:18:17 +0900

    dynamic memory allocate-declare by new-1

--- Chapter1-1/main.cpp
+++ Chapter1-1/main.cpp
@@ -19,11 +19,17 @@ int main()
 //    var = 7;
 
     // 주소를 반납, 4바이트(int size)에 맞춰서 os에게서 메모리르 받아오고 우리에게 주소를 알려줌. 그렇기에 포인터(int 타입)로 받아줘야함.
-    int *ptr = new int;
+//    int *ptr = new int;
 
     // 위에서 받은 메모리 주소에 역참조(de-referencing)로 값을 할당함.
-    *ptr = 7;
+//    *ptr = 7;
 
-    return 0;
+    // 위의 2줄을 한 번에 초기화할 수도 있음.
+    int *ptr = new int{ 7 };
+
+    cout << ptr << endl;
+    cout << *ptr << endl;
+
+   return 0;
 
 }
