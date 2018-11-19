f2a80647ce1df9168f68a4e18ccce43db678d3d5
dean-kim <keunu07@gmail.com>
2018-11-20 08:11:50 +0900

    dynamic memory allocate-declare by new

--- Chapter1-1/main.cpp
+++ Chapter1-1/main.cpp
@@ -12,9 +12,17 @@ using namespace std;
 int main()
 {
 
-    char c = 'Q';
-    cout << &c << endl;
-    cout << *&c << endl;
+//    int array[1000000]; // 정적인 메모리는 stack, 동적인 메모리는 heap.
+
+    // 아래의 new로 선언하는 것과 같은 효과
+//    int var;
+//    var = 7;
+
+    // 주소를 반납, 4바이트(int size)에 맞춰서 os에게서 메모리르 받아오고 우리에게 주소를 알려줌. 그렇기에 포인터(int 타입)로 받아줘야함.
+    int *ptr = new int;
+
+    // 위에서 받은 메모리 주소에 역참조(de-referencing)로 값을 할당함.
+    *ptr = 7;
 
     return 0;
 
