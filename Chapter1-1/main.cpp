2b6efc23784b447e8fedd89c75b89957b9872edc
dean-kim <keunu07@gmail.com>
2018-11-20 08:56:34 +0900

    dynamic allocate array-initialization

--- Chapter1-1/main.cpp
+++ Chapter1-1/main.cpp
@@ -16,7 +16,12 @@ int main()
 
     cin >> length;
 
-    int *array = new int[length];
+//    int *array = new int[length];
+    // 0으로 초기화
+//    int *array = new int[length]();
+    // uniform initialization
+    // 값을 할당해서 초기화할 때는 사이즈 할당을 잘 생각해야 됨. 초기화하는 사이즈보다는 크게 해야됨.
+    int *array = new int[length]{11, 22, 33, 44, 55, 66};
 
     array[0] = 1;
     array[1] = 2;
