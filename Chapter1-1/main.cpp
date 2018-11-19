7ca4c83afa35efdc9880b2c8614478f0f1aa684a
dean-kim <keunu07@gmail.com>
2018-11-19 10:07:13 +0900

    pointer cal & array indexing

--- Chapter1-1/main.cpp
+++ Chapter1-1/main.cpp
@@ -8,42 +8,18 @@
 
 using namespace std;
 
-struct MyStruct
-{
-    int array[5] = { 9, 7 ,5, 3, 1};
-};
 
-void doSomething(MyStruct *ms)
-{
-    cout << sizeof((*ms).array) << endl;
-}
-
-// void printArray(int *array) -> same result
-void printArray(int array[])
-{
-    cout << sizeof(array) << endl; // 8, because of array recognize as pointer
-    cout << *array << endl;
-
-    *array = 100;
-}
 
 int main()
 {
 
-    MyStruct ms;
+    int value = 7;
+    int *ptr = &value;
 
-    cout << ms.array[0] << endl;
-    cout << sizeof(ms.array) << endl;
-
-    doSomething(&ms);
-
-    char name[] = "JackJack";
-
-    cout << name << endl;
-    cout << &name[0] << endl;
-
-    // de-referencing
-    cout << *name << endl;
+    cout << uintptr_t(ptr - 1) << endl;
+    cout << uintptr_t(ptr) << endl;
+    cout << uintptr_t(ptr + 1) << endl;
+    cout << uintptr_t(ptr + 2) << endl;
 
     return 0;
 
