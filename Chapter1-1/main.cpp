fe73798c9444cbae54db21a33e0cbce02358df43
dean-kim <keunu07@gmail.com>
2018-11-21 09:08:36 +0900

    dynamic allocate array-1

--- Chapter1-1/main.cpp
+++ Chapter1-1/main.cpp
@@ -12,28 +12,15 @@ using namespace std;
 int main()
 {
 
-    int length;
+    int fixedArray[] = { 1, 2, 3, 4, 5 };
 
-    cin >> length;
+    int *array = new int[3]{ 1, 2, 3 };
+    // compile fail
+    // int *array = new int[]{ 1, 2, 3, 4, 5 };
 
-//    int *array = new int[length];
-    // 0으로 초기화
-//    int *array = new int[length]();
-    // uniform initialization
-    // 값을 할당해서 초기화할 때는 사이즈 할당을 잘 생각해야 됨. 초기화하는 사이즈보다는 크게 해야됨.
-    int *array = new int[length]{11, 22, 33, 44, 55, 66};
-
-    array[0] = 1;
-    array[1] = 2;
-    array[2] = 3;
-
-    for (int i = 0; i < length; ++i)
-    {
-        cout << (uintptr_t)&array[i] << endl;
-        cout << array[i] << endl;
-    }
-
-    delete [] array;
+    // resizing - 직접적으로 되지는 않음. 더 큰메모리를 받아오고 기존의 배열을 복사한 뒤 요소를 추가해야함.
+    
+    delete[] array;
 
     return 0;
 
